12dea32d38c528619bbdfff4817d81262df9df58
tetsuyayamamoto0324 <manpuku769@gmail.com>
2025-08-04 21:43:02 +0900

    完成A

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,13 @@ import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
